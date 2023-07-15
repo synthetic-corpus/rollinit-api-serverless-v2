52f72d14e62a0b8c03ba87318c810fc4f4204891
Joel G <joel@joelgonzaga.com>
2023-07-15 15:48:06 -0700

    added the tent lambda crud.

--- src/functions/tents/tenthandlers.ts
+++ src/functions/tents/tenthandlers.ts
@@ -6,40 +6,41 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } f
 import { formatJSONResponse } from "@libs/api-gateway";
 import { tentHTTP } from "./schema";
 import { middyfy } from "@libs/lambda";
+import { getUserId } from "@functions/auth/authUtils";
+import * as tents from '@business/tent.logic'
 
 const tentCreate: ValidatedEventAPIGatewayProxyEvent<typeof tentHTTP> = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    return formatJSONResponse({
-        message: "tent create called!",
-        event
-    })
+    const user_id = getUserId(event)
+    const body = JSON.parse(event.body)
+    const returnThis = await tents.createTent(user_id,body)
+    return formatJSONResponse(returnThis.code,returnThis.data)
 }
 
 const tentRetrieve: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    return formatJSONResponse({
-        message: "retrieve tent was called!",
-        event
-    })
+    const user_id = getUserId(event)
+    const id = event.pathParameters.id
+    const returnThis = await tents.getTent(user_id,id)
+    return formatJSONResponse(returnThis.code,returnThis.data)
 }
-
 const tentRetrieveAll: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    return formatJSONResponse({
-        message: "retrieve tent was called!",
-        event
-    })
+    const user_id = getUserId(event)
+    const returnThis = await tents.getAllTents(user_id)
+    return formatJSONResponse(returnThis.code,returnThis.data)
 }
 
 const tentPatch: ValidatedEventAPIGatewayProxyEvent<typeof tentHTTP> = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    return formatJSONResponse({
-        message: "tent patch called!",
-        event
-    })
+    const user_id = getUserId(event)
+    const id = event.pathParameters.id
+    const patch = JSON.parse(event.body)
+    const returnThis = await tents.patchTent(user_id,id,patch)
+    return formatJSONResponse(returnThis.code,returnThis.data)
 }
 
 const tentDelete: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    return formatJSONResponse({
-        message: "delete tent was called!",
-        event
-    })
+    const user_id = getUserId(event)
+    const id = event.pathParameters.id
+    const returnThis = await tents.deleteTent(user_id,id)
+    return formatJSONResponse(returnThis.code,returnThis.data)
 }
 
 export const tentCreateMiddy = middyfy(tentCreate)
